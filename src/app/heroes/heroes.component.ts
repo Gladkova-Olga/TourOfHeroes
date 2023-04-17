cddad44a6fa0ac04eb2d4ef81aa86caf75aa995b
Lisla Fox <lisla@Lislas-MBP.lan>
2023-04-17 12:51:04 +0300

    navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { MessageService } from '../message.service';
 })
 
 export class HeroesComponent implements OnInit {
-selectedHero? : Hero;
 heroes: Hero[] = [];
 
 constructor(
@@ -18,10 +17,6 @@ constructor(
   private messageService: MessageService
 ){}
 
-onSelected(hero: Hero): void {
-  this.selectedHero = hero;
-  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-} 
 
 getHeroes(): void {
   this.HeroService.getHeroes()
